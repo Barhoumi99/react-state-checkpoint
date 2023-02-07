4c7b871d4c7e9453300ca435fccc7d55f123d028
Wael Barhoumi <wael.barhoumi14@gmail.com>
2023-02-07 23:54:04 +0100

    no setInterval yet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+import avatar from './avatar.jpg'
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullName: 'Wassim Barhoumi',
+      bio: 'lectus nulla at volutpat diam ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur libero id',
+      imgSrc: avatar,
+      profession: 'Web developer',
+      displayed: true
+    }
+  }
+  handleClick = () => {
+    this.setState({
+      displayed: !this.state.displayed
+      
+    })
+  }
+  render() {
+    return(
+      <div>
+        <button onClick={this.handleClick}>Show/Hide</button>
+        {this.state.displayed && 
+          <div>
+            <img src={this.state.imgSrc} width = '300px' alt = 'avatar illustration' />
+            <h2>{this.state.fullName}</h2>
+            <h4>{this.state.profession}</h4>
+            <p>{this.state.bio}</p>
+          </div>
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
