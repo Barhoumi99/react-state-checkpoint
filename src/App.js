e09f9ac12a6ded56470fe1b53b084d5c17535841
Wael Barhoumi <wael.barhoumi14@gmail.com>
2023-02-12 19:23:59 +0100

    checkpoint completed

--- src/App.js
+++ src/App.js
@@ -9,13 +9,19 @@ class App extends Component {
       bio: 'lectus nulla at volutpat diam ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur libero id',
       imgSrc: avatar,
       profession: 'Web developer',
-      displayed: true
+      displayed: true,
+      elapsedTime: 0,
     }
   }
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({elapsedTime: this.state.elapsedTime + 1});
+    }, 1000);
+  } 
   handleClick = () => {
     this.setState({
-      displayed: !this.state.displayed
-      
+      displayed: !this.state.displayed,
+      elapsedTime: 0
     })
   }
   render() {
@@ -24,7 +30,8 @@ class App extends Component {
         <button className = 'btn' onClick={this.handleClick}>Show/Hide</button>
         {this.state.displayed && 
           <div className='card'>
-            <img src={this.state.imgSrc} width = '300px' alt = 'avatar illustration' />
+            <div>{this.state.elapsedTime}</div>
+            <img src={this.state.imgSrc} width = '200px' alt = 'avatar illustration' />
             <h2>{this.state.fullName}</h2>
             <h4>{this.state.profession}</h4>
             <p>{this.state.bio}</p>
